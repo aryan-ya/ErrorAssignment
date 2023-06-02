f547675ad12d29c2d77abbdf824d944853708cf9
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-06-02 07:56:30 +0530

    Create Presenceclouser.js

---
+++ Presenceclouser.js
@@ -0,0 +1,20 @@
+// 9. Check the presence using closures.
+// Create a numberChecker function that takes an array of numbers as an argument and returns a function. The
+// returned function should take another number as an argument and return true if the number is in the array, and
+// false otherwise.
+
+
+
+
+function numberChecker(numbers) {
+    return function(number) {
+      return numbers.includes(number);
+    };
+  }
+  
+
+  var checkNumber = numberChecker([1, 2, 3, 4, 5]);
+  
+  console.log(checkNumber(3)); 
+  console.log(checkNumber(6));
+  
